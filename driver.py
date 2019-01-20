bd7fca9cd6775aaad8e54bb2b2a26399f389f817
borokov <borokov@protonmail.com>
2019-01-20 16:29:09 +0000

    Send frame to influxDB

--- driver.py
+++ driver.py
@@ -2,6 +2,9 @@ import usb.core, usb.util, usb.control
 import crc16
 import datetime
 import time
+from influxdb import InfluxDBClient
+import json
+
 
 def getCommand(cmd):
     cmd = cmd.encode('utf-8')
@@ -35,6 +38,10 @@ def get_frame(timeout=100):
                         error = True
                 i+=1
 
+            if len(res) < 3:
+                error = True
+                break;
+
             crc = res[len(res)-3:-1]
             values = res[:-3]
 
@@ -52,6 +59,7 @@ def get_frame(timeout=100):
                 ok = True
 
         except Exception as e:
+            error = True
             print(repr(e))
         
 
@@ -65,38 +73,51 @@ class QPGS0(object):
 
         value_array = frame.split(' ')
 
-        self.a = value_array[0]
-        self.b = value_array[1]
-        self.c = value_array[2]
-        self.d = value_array[3]
-        self.e_voltage_1 = value_array[4]
-        self.f = value_array[5]
-        self.g_voltage_2 = value_array[6]
-        self.h_voltage_3 = value_array[7]
-        self.i = value_array[8]
-        self.j = value_array[9]
-        self.k = value_array[10]
-        self.l = value_array[11]
-        self.m = value_array[12]
-        self.o = value_array[14]
-        self.p = value_array[15]
-        self.q = value_array[16]
-        self.r = value_array[17]
-        self.s = value_array[18]
-        self.t = value_array[19]
-        self.u = value_array[20]
-        self.v = value_array[21]
-        self.w = value_array[22]
-        self.x = value_array[23]
-        self.y = value_array[24]
-        self.z = value_array[25]
-        self.z1 = value_array[26]
+        self.a = int(value_array[0]) # 1
+        self.b = value_array[1] # 92931712101861 
+        self.c = value_array[2] # L
+        self.d = float(value_array[3]) # 0
+        self.e_voltage_1 = float(value_array[4]) # =g: Tension de sortie onduleur [V]
+        self.f = float(value_array[5]) # =h: Tension batterie ? Frequence sortie ?
+        self.g_voltage_2 = float(value_array[6]) # =e: Tension de sortie onduleur [V]
+        self.h_voltage_3 = float(value_array[7]) # =f: Tension batterie ? Frequence sortie ?
+        self.i = int(value_array[8]) # =q power [VA] en sortie
+        self.j = int(value_array[9]) # =r power [W] en sortie
+        self.k = float(value_array[10]) # charge en %
+        self.l = float(value_array[11]) # Frequence sortie ? (~50)
+        self.m = int(value_array[12]) # =p intensité solaire ? [A]
+        self.o = float(value_array[14]) # puissance solaire ? [W]
+        self.p = int(value_array[15]) # =o
+        self.q = int(value_array[16]) # =i: power [VA] en sortie
+        self.r = int(value_array[17]) # =j: power [W] en sortie
+        self.s = int(value_array[18]) # =k
+        self.t = value_array[19] # 10100010
+        self.u = int(value_array[20]) # 0
+        self.v = int(value_array[21]) # 1
+        self.w = int(value_array[22]) # 50
+        self.x = int(value_array[23]) # 240
+        self.y = int(value_array[24]) # 30
+        self.z = int(value_array[25]) # =m
+        self.z1 = int(value_array[26]) # 0
 
     def __str__(self):
         str = ""
         for member in sorted(self.__dict__):
             str += ("{}: {}\n".format(member, self.__dict__[member]))
         return str
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    def send(self):
+        pointValues = {
+            "measurement": "solar.QPSG0",
+            "fields": self.__dict__,
+        }
+        print(pointValues)
+
+        client = InfluxDBClient("plusdepanda.no-ip.org", 8086, "root", "root", "EDF")
+        client.write_points([pointValues])
 
 
 vendorId = 0x0665
@@ -114,9 +135,10 @@ print("USB connected ---")
 
 
 while True:
-    sendCommand('QPGS0')
-    res = get_frame()
-    #print(res)
-    print(QPGS0(res))
-    t = datetime.datetime.now()
-    #print("{}: {}".format(t,res))
+    try:
+        sendCommand('QPGS0')
+        res = get_frame()
+        qpgs0 = QPGS0(res)
+        qpgs0.send()
+    except:
+        pass
